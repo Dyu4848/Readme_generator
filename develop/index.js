37a4261c2672b63d81dfd067f887681bb5c414fc
David Yu <dyu4848@gmail.com>
2023-01-12 16:47:57 -0500

    created markdown and index

--- develop/index.js
+++ develop/index.js
@@ -1,43 +1,60 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
-{
+    {
+        type: 'input',
+        message: 'Enter title of project',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter table of contents',
+        name: 'content',
+    },
+    {
+        type: 'input',
+        message: 'How did you install?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'what is the usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'what licensing was used for this project?',
+        name: 'license',
+    },
+    {
     type: 'input',
-    message: 'enter main title',
-    name: 'title',
-},
-{
-    type: 'input',
-    message: 'enter title text',
-    name: 'text',
-},
-{
-    type: 'input',
-    message: 'enter use story',
-    name: 'story'
-},
-{
-    type: 'input',
-    message: 'enter accceptance criteria',
-    name: 'acceptance',
-},
-{
-    type: 'input',
-    message: 'enter helpful guidelines',
-    name: 'started',
-},
-{
-    type: 'input',
-    message: 'enter the grading requirements',
-    name: 'grading',
-},
-
+    message: 'who conrtibuted to this project?',
+    name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'is there a test?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Do you have any questions?',
+        name: 'question',
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data))
+}
 
 // TODO: Create a function to initialize app
 function init() {
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,25 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `#{data.title}
-${data.name}
-${data.name}
-${data.text}
-${data.story}
-${data.acceptance}
-${data.started}
-${data.grading}`;
+  return `
+  ## Project Title
+  ${data.title}
+  
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  ${data.content}
+  -Title
+  -Description
+  -Installation
+  -Usage
+  -License
+  -Contribution
+  -Test
+  -Question
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.license}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## Question
+  ${data.question}
+`;
 }
 
 module.exports = generateMarkdown;
